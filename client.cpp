1e8e058e2aac80e1c0255f2f7176e919a167b576
Zheng Yunkun <2240324227@qq.com>
2024-08-21 20:05:05 +0800

    Add more description

--- client.cpp
+++ client.cpp
@@ -1,7 +1,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 #include "util.h"
 
@@ -10,20 +10,27 @@ int main()
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     errIf(sockfd == -1, "socket create failed...");
 
-    struct sockaddr_in server_addr;
+    struct sockaddr_in server_addr; // 服务器地址
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(8080);
 
+    // sockaddr和sockaddr_in大小相同，sockaddr_in有padding，都是16字节
     errIf(connect(sockfd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1, "socket connect failed...");
+    /*
+     *@sockfd: 套接字文件描述符
+     *@server_addr: 服务器地址和端口等信息
+     *@sizeof(server_addr): 信息长度
+     */
 
     while (true)
     {
         char buf[1024];
         bzero(&buf, sizeof(buf));
-        scanf("%s", buf);
+        scanf("%s", buf); // 客户端阻塞等待输入
         ssize_t writeBytes = write(sockfd, buf, sizeof(buf));
+        // 客户端发送数据: write -> 内核发送缓冲区 -> 网络协议栈 -> 网络传输
         if (writeBytes == -1)
         {
             printf("Socket already disconnected, cannot write anymore...");
--- server.cpp
+++ server.cpp
@@ -1,15 +1,13 @@
 /* 
  * @Author: Zheng Yunkun
  * @Date: 2024-8-1 00:48:45
- * @LastEditTime: 2024-8-20 19:54:32
+ * @LastEditTime: 2024-8-21 19:28:32
  * @LastEditors: Larry Kinn
  * @Description: Start up
  */
 
 #include <sys/socket.h>
-// 套接字接口
 #include <arpa/inet.h>
-// 绑定套接字到一个IP地址和接口上
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -21,7 +19,7 @@ int main()
     /*
      *@AF_INET: IPv4 (Address Family Internet -- AF_INET)
      *@SOCK_STREAM: TCP (Stream socket) 表示流格式，适用于TCP协议
-     *@0： 传输协议，根据前面的两个参数自动推导协议类型，设置为IPPROTO_TCP或者IPPTOTO_UDP，分别表示TCP和UDP，TCP是面向连接的协议，UDP无需连接
+     *@0： 使用默认传输协议
      */
     errIf(sockfd == -1, "socket create failed...");
 
@@ -30,18 +28,19 @@ int main()
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(8080);
+    server_addr.sin_port = htons(8080); // 端口号让同一网络设备能够同时处理多个网络连接
 
     errIf((bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1), "socket bind failed...");
     // Give the socket FD the local address ADDR
     errIf((listen(sockfd, SOMAXCONN) == -1), "socket listen failed...");
-    // 监听这个socket端口，SOMAXCONN表示最大连接数
+    // 操作系统会分配一个连接队列，大小是SOMAXCONN，当有连接请求到达时，内核会将其放入队列中
 
     // 接受一个客户端连接
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr); // accept需要写入客户端socket长度
     bzero(&client_addr, client_addr_len);
-    int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len); // 客户端socket的长度
+    int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+    // client_sockfd表示一个已经建立连接的客户端
     errIf(client_sockfd == -1, "socket accept failed...");
 
     // 接受完请求后执行
@@ -55,8 +54,7 @@ int main()
         // Define buffer
         bzero(&buf, sizeof(buf)); // 清空缓冲区
         ssize_t readBytes = read(client_sockfd, buf, sizeof(buf));
-        // 从客户端socket读到缓冲区
-        // 客户端通过accept函数获得的
+        // 网络传输 -> 内核缓冲区 -> read
 
         if (readBytes > 0)
         {
