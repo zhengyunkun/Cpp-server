d3b0ceb949f975a869f1dd57face8acc3ce6fda1
Zheng Yunkun <2240324227@qq.com>
2024-08-23 14:35:52 +0800

    InetAddress updated

--- InetAddress.cpp
+++ InetAddress.cpp
@@ -1,7 +1,7 @@
 #include "InetAddress.h"
 #include <string.h>
 
-InetAddress::InetAddress() : addr_len(sizeof(addr)) // 初始化成员列表
+InetAddress::InetAddress() : addr_len(sizeof(addr))
 {
     bzero(&addr, sizeof(addr));
 }
@@ -10,9 +10,9 @@ InetAddress::InetAddress(const char* ip, uint16_t port) : addr_len(sizeof(addr))
 {
     bzero(&addr, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(ip); //10进制ip地址转换为网络字节序
-    addr.sin_port = htons(port); //16位主机字节序转换为网络字节序
-    addr_len = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr(ip);  // 将10进制的ip地址字符串转换成二进制的网络字节序
+    addr.sin_port = htons(port);           // 将16位无符号整数转换为网络字节序
+    // addr_len = sizeof(addr); // 多余
 }
 
 InetAddress::~InetAddress()
--- InetAddress.h
+++ InetAddress.h
@@ -6,9 +6,11 @@
 class InetAddress
 {
     public:
-        struct sockaddr_in addr; 
+        struct sockaddr_in addr;
         socklen_t addr_len;
         InetAddress();
-        InetAddress(const char* ip, uint16_t port); //ip是字符串，port是16位无符号整数
+        InetAddress(const char* ip, uint16_t port);  
+        // uint16_t是16位无符号整数，范围0x0000-0xFFFF正好用来表示端口
+        // _t表示C/C++中的标准类型
         ~InetAddress();
 };
