ceafa8dab2e6d7684e6131b585bf9d19c8d6124d
Zheng Yunkun <2240324227@qq.com>
2024-08-23 23:38:55 +0800

    Update

--- server.cpp
+++ server.cpp
@@ -6,104 +6,13 @@
  * @Description: Linux Server Start up
  */
 
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <vector>
-#include "util.h"
-#include "Epoll.h"
-#include "Socket.h"
-#include "Channel.h"
-#include "InetAddress.h"
-
-#define MAX_EVENTS 1024
-#define READ_BUFFER 1024
-
-void handleReadEvent(int);
+#include <src/EventLoop.h>
+#include <src/Server.h>
 
 int main()
 {
-    Socket* server_sock = new Socket(); // 调用Socket的构造函数，创建一个socket对象
-    InetAddress* server_addr = new InetAddress("127.0.0.1", 8080);
-
-    server_sock->bind(server_addr);
-    server_sock->listen();
-    server_sock->setNonBlocking();
-
-    Epoll* ep = new Epoll();
-    // ep->addFd(server_sock->getFd(), EPOLLIN | EPOLLET);
-    Channel* serverChannel = new Channel(ep, server_sock->getFd());
-    serverChannel->enableReading();
-
-    while (true)
-    {
-        std::vector<Channel*> activeChannels = ep->poll();
-        int nfds = activeChannels.size();
-
-        for (int i = 0; i < nfds; i ++ )
-        {
-            int chfd = activeChannels[i]->getFd();
-            if (chfd == server_sock->getFd())
-            {
-                InetAddress* client_addr = new InetAddress();
-                int client_fd = server_sock->accept(client_addr);
-                Socket* client_sock = new Socket(client_fd);
-                client_sock->setNonBlocking();
-
-                char* clientIp = inet_ntoa(client_addr->addr.sin_addr);
-                int clientPort = ntohs(client_addr->addr.sin_port);
-                printf("Client id: %d, Client connected: %s, Port: %d\n", 
-                        client_sock->getFd(), clientIp, clientPort);
-                
-                // 引入Channel类，将client_sock的fd和epoll进行绑定，封装部分epoll的操作
-                Channel* clientChannel = new Channel(ep, client_sock->getFd());
-                clientChannel->enableReading();
-            }
-            else if (activeChannels[i]->getRevents() & EPOLLIN)
-            {
-                handleReadEvent(chfd);
-            }
-            else printf("Unknown event...\n");
-        }
-    }
-    
-    delete server_sock;
-    delete server_addr;
-    delete ep;
+    EventLoop* loop = new EventLoop();
+    Server* server = new Server(loop);
+    loop->loop();   // 事件驱动开始
     return 0;
-}
-
-void handleReadEvent(int sockfd)
-{
-    char buf[READ_BUFFER];
-    bool dataRead = false;
-    while (true)
-    {
-        bzero(&buf, sizeof(buf));
-        ssize_t readBytes = read(sockfd, buf, sizeof(buf));
-        if (readBytes > 0)
-        {
-            printf("Message from client fd %d: %s\n", sockfd, buf);
-            write(sockfd, buf, sizeof(buf));
-            dataRead = true;
-        }
-        else if (readBytes == -1 && errno == EINTR) // 被外部信号中断，继续读取
-        {
-            printf("Continuing reading...\n");
-            continue;
-        }
-        else if (readBytes == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK)))
-        {
-            if (!dataRead) printf("Finish reading, errno: %d\n", errno);
-            break;
-        }
-        else if (readBytes == 0)
-        {
-            printf("Client fd %d disconnected...\n", sockfd);
-            close(sockfd);
-            break;
-        }
-    }
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -5,6 +5,8 @@
 #include <functional>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
 
 #define READ_BUFFER 1024
 
@@ -27,16 +29,56 @@ Server::~Server()
 {
 }
 
+// 针对客户端的读事件处理函数
 void Server::handleReadEvent(int sockfd)
 {
     char buf[READ_BUFFER];
+    bool dateRead = false;
     while (true)
     {
         bzero(&buf, READ_BUFFER);
         ssize_t readBytes = read(sockfd, buf, READ_BUFFER);
         if (readBytes > 0)
         {
-            // printf("Client id: %d, Client connected: %s, Port: %d\n", );
+            printf("Message from client fd %d: %s\n", sockfd, buf);
+            write(sockfd, buf, READ_BUFFER);
+            dateRead = true;
+        }
+        else if (readBytes == -1 && errno == EINTR)
+        {
+            printf("Continuing reading...\n");
+            continue;
+        }
+        else if (readBytes == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK)))
+        {
+            if(!dateRead) printf("Finish reading, errno: %d\n", errno);
+            break;
+        }
+        else if (readBytes == 0)
+        {
+            printf("Client fd %d disconnected...\n", sockfd);
+            close(sockfd);
+            break;
         }
     }
+}
+
+// 针对本地服务器的新连接事件处理函数
+void Server::newConnection(Socket* server_sock)
+{
+    InetAddress* client_addr = new InetAddress();
+    int client_fd = server_sock->accept(client_addr);
+    Socket* client_sock = new Socket(client_fd);
+    client_sock->setNonBlocking();
+
+    char* clientIp = inet_ntoa(client_addr->addr.sin_addr);
+    int clientPort = ntohs(client_addr->addr.sin_port);
+    printf("Client id: %d, Client connected: %s, Port: %d\n", 
+            client_sock->getFd(), clientIp, clientPort);
+
+
+    Channel* clientChannel = new Channel(loop, client_sock->getFd());
+    std::function<void()> cb = std::bind(&Server::handleReadEvent, this, client_sock->getFd());
+    clientChannel->setCallback(cb);
+    clientChannel->enableReading();
 }
